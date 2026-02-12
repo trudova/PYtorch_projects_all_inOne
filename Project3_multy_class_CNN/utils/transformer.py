3d19928bfa39fa423e3a0e2f8f24365fe000479c
Liubov Bradford <bradfoli@Liubovs-MacBook-Air.local>
2026-02-12 11:22:55 -0700

    project 3 set up

---
+++ Project3_multy_class_CNN/utils/transformer.py
@@ -0,0 +1,17 @@
+import torchvision.transforms as transforms
+
+
+def transform():
+    transform = transforms.Compose(
+        [
+            transforms.Resize(512),
+            transforms.RandomRotation(10),
+            transforms.RandomVerticalFlip(),
+            transforms.RandomHorizontalFlip(),
+            transforms.Resize((64, 64)),
+            # transforms.Grayscale(num_output_channels=1),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        ]
+    )
+    return transform

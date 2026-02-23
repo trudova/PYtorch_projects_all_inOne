692b92786a4b781988d9986e666ab0cb2a8ad68b
Liubov Bradford <bradfoli@Liubovs-MacBook-Air.local>
2026-02-23 14:26:16 -0700

    proj structure setup

---
+++ Project7_DensNet_transfer/utils/transformer.py
@@ -0,0 +1,19 @@
+import torchvision.transforms as transforms
+
+
+def transform():
+    transform = transforms.Compose(
+        [
+            transforms.Resize((256, 256)),
+            transforms.RandomInvert(),
+            transforms.RandomRotation(10),
+            transforms.RandomHorizontalFlip(),
+            transforms.ColorJitter(
+                brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1
+            ),
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ]
+    )
+    return transform
